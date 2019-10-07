09938ba86afe8448a21821e15a0aca20fe0705e8
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-10-07 15:20:30 -0500

    added return date info

--- src/components/display.js
+++ src/components/display.js
@@ -3,11 +3,12 @@ import React from 'react';
 class Display extends React.Component {
   state = {
     time: {
-      seconds: 30,
-      minutes: 5,
-      hours: 1,
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
     },
     dispalyTime: '',
+    timeReturning: '',
   }
 
   setTimer = (time) => {
@@ -75,17 +76,60 @@ class Display extends React.Component {
     this.setState({ dispalyTime: `${hoursString}:${minutesString}:${secondsString}` });
   }
 
+  formateRetrunDate = (date) => {
+    const weekDay = this.days[date.getDay()];
+    const day = date.getDate();
+    const month = this.months[date.getMonth()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'pm' : 'am';
+
+    hours %= 12;
+    if (!hours) {
+      hours = 12;
+    }
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    const formattedTime = `I'll be back on ${weekDay}, ${month} ${day}, at ${hours}:${minutes} ${ampm}`;
+    return formattedTime;
+  }
+
+  calculateReturnTime = () => {
+    const { time } = this.state;
+    const totalMinutes = (time.hours * 60) + time.minutes;
+    const totalSeconds = (totalMinutes * 60) + time.seconds;
+    const totalMiliseconds = totalSeconds * 1000;
+
+    const currentTime = new Date();
+    const timeNow = new Date().getTime();
+    const arrivalDate = timeNow + totalMiliseconds;
+
+    const timeReturning = new Date(arrivalDate);
+
+    const formattedTime = this.formateRetrunDate(timeReturning);
+
+    this.setState({ timeReturning: formattedTime });
+  }
+
   componentDidMount() {
     this.countdownInterval = null;
+    this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     this.formatTime();
+    this.calculateReturnTime();
     this.startTimer();
   }
 
   render() {
     const { dispalyTime } = this.state;
+    const { timeReturning } = this.state;
     return (
       <div className="Display">
         <div>{dispalyTime}</div>
+        <div>{timeReturning}</div>
       </div>
     );
   }
